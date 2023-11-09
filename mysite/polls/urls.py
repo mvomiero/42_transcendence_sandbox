561101bede37d1e58bf3e6e176fddf54fa9fe765
Marco Vomiero <mvomiero@student.42berlin.de>
2023-11-09 12:37:14 +0100

    tutorial 6, added static files

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,14 +3,12 @@ from django.urls import path
 # impoting views from the same directory
 from . import views
 
-app_name = "polls"
-
 # The path() function is three arguments, two required: route and view. It's an empty string because it's the root URL.
 # The third argument, is a name tag used for referencing the view in other parts of the Django app.
-app_name = "polls"
+app_name = 'polls'
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
